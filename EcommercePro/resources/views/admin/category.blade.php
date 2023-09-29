9ce7f9d7b2517e73c7c2e2f87c926702330b7aeb
marzanulhoque <marzanulru17@gmail.com>
2023-09-29 10:35:49 +0600

    Added Catagory Data From Admin Panel

--- EcommercePro/resources/views/admin/category.blade.php
+++ EcommercePro/resources/views/admin/category.blade.php
@@ -3,6 +3,22 @@
   <head>
     <!-- Required meta tags -->
     @include('admin.css')
+
+    <style>
+        .div_center{
+
+            text-align: center;
+            padding-top:40px;
+        }
+        .h2_font{
+            font-size: 40px;
+            padding-bottom: 40px;
+        }
+        .text_color{
+            color: black;
+        }
+    </style>
+
   </head>
   <body>
     <div class="container-scroller">
@@ -31,7 +47,32 @@
         <!-- partial:partials/_navbar.html -->
         @include('admin.navbar')
         <!-- partial -->
-        @include('admin.body')
+        <div class="main-panel">
+            <div class="content-wrapper">
+
+                @if (session()->has('message'))
+                    <div class="alert alert-success">
+
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true" >x</button>
+                        {{ session()->get('message') }}
+
+                    </div>
+                @endif
+
+                <div class="div_center">
+
+                    <h2 class="h2_font"> Add Category </h2>
+                    <form action="{{ url('add_category') }}" method="POST">
+                        @csrf
+                        <input class="text_color"  type="text" name="category" placeholder="Write Category Name">
+                        <br>
+                        <br>
+                        <input type="submit" class="btn btn-primary" name="submit" value="Add Category">
+
+                    </form>
+                </div>
+            </div>
+        </div>
         <!-- main-panel ends -->
       </div>
       <!-- page-body-wrapper ends -->
